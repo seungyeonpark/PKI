3bc3012289c0fbefbe412c128c58b38670b962b2
DESKTOP-Q200243\User <siaayeon@gmail.com>
2023-09-20 10:28:03 +0900

    Initial commit

---
+++ src/test/java/pki/CertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package pki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
