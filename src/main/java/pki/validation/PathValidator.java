4de226bca2816290941fd13c55d873819ca02f97
DESKTOP-Q200243\User <siaayeon@gmail.com>
2023-09-22 17:38:35 +0900

    feat: add path validator

---
+++ src/main/java/pki/validation/PathValidator.java
@@ -0,0 +1,42 @@
+package pki.validation;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.security.cert.*;
+import java.util.*;
+
+public class PathValidator {
+
+    public void validateCertPath(X509Certificate rootCaCert, X509Certificate caCert, X509Certificate leafCert) throws CertificateException, NoSuchProviderException, InvalidAlgorithmParameterException, NoSuchAlgorithmException {
+
+        Security.addProvider(new BouncyCastleProvider());
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509", "BC");
+
+        // 1. CertPath
+        List<Certificate> certList = new ArrayList<>();
+        certList.add(caCert);
+        certList.add(leafCert);
+
+        CertPath certPath = certificateFactory.generateCertPath(certList);
+
+        // 2. TrustAnchor
+        Set<TrustAnchor> trustAnchorSet = Collections.singleton(new TrustAnchor(rootCaCert, null));
+
+        // 3. PKIXParameters
+        PKIXParameters pkix = new PKIXParameters(trustAnchorSet);
+        pkix.setRevocationEnabled(false);
+
+        // 4. Validate
+        CertPathValidator certPathValidator = CertPathValidator.getInstance("PKIX", "BC");
+        try {
+            certPathValidator.validate(certPath, pkix);
+        } catch (CertPathValidatorException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
