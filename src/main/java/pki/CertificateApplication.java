c336276e4ef52d02c4c88b0b80c6967930aaf734
DESKTOP-Q200243\User <siaayeon@gmail.com>
2023-09-22 17:37:33 +0900

    fix: dependencies

--- src/main/java/pki/CertificateApplication.java
+++ src/main/java/pki/CertificateApplication.java
@@ -1,13 +1,9 @@
 package pki;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CertificateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CertificateApplication.class, args);
+
 	}
 
 }
