1948b322db250b6a8807b5317b191df279379209
DESKTOP-Q200243\User <siaayeon@gmail.com>
2023-09-25 17:32:42 +0900

    fix: csr manager

--- src/main/java/pki/csr/CsrManager.java
+++ src/main/java/pki/csr/CsrManager.java
@@ -7,27 +7,33 @@ import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
-import pki.common.Constants;
-import pki.common.KeyUtil;
+import pki.common.util.KeyUtil;
 
 import java.io.*;
 import java.security.*;
 
 public class CsrManager {
 
-    public void generateCsr() throws OperatorCreationException, IOException {
-        X500Name subjectName = new X500Name(Constants.subjectDN);
+    private String csrSigningAlg;
+
+    private String subjectDn;
+
+    public CsrManager(String csrSigningAlg, String subjectDn) {
+        this.csrSigningAlg = csrSigningAlg;
+        this.subjectDn = subjectDn;
+    }
+
+    public byte[] getCsr() throws OperatorCreationException, IOException {
+        X500Name subjectName = new X500Name(subjectDn);
         KeyPair subjectKeyPair = KeyUtil.generateRSAKeyPair();
         SubjectPublicKeyInfo subjectPublicKeyInfo = SubjectPublicKeyInfo.getInstance(subjectKeyPair.getPublic().getEncoded());
 
-        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(Constants.csrSigningAlg);
+        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(csrSigningAlg);
         ContentSigner signer = signerBuilder.build(subjectKeyPair.getPrivate());
 
         PKCS10CertificationRequestBuilder csrBuilder = new PKCS10CertificationRequestBuilder(subjectName, subjectPublicKeyInfo);
         PKCS10CertificationRequest csr = csrBuilder.build(signer);
 
-        FileOutputStream fo = new FileOutputStream(Constants.csrFilePath);
-        fo.write(csr.getEncoded());
-        fo.close();
+        return csr.getEncoded();
     }
 }
